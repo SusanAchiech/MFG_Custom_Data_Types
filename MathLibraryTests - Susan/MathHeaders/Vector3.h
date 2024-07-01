137076eee2e81c0d81b1b34f404dcf4f8c114a02
Susan Achiech <127722179+SusanAchiech@users.noreply.github.com>
2024-07-01 10:53:23 +0300

    Vector3 returned no errors

--- MathLibraryTests - Susan/MathHeaders/Vector3.h
+++ MathLibraryTests - Susan/MathHeaders/Vector3.h
@@ -1,5 +1,8 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
 #pragma once
 #include <string>
+#include <cmath>
 
 namespace MathClasses
 {
@@ -18,12 +21,47 @@ namespace MathClasses
         }
 
         //Additional operator
-        bool operator+(const Vector3& other) const
+        Vector3 operator+(const Vector3& other) const
         {
             return Vector3(x + other.x, y + other.y, z + other.z);
         }
 
         // Scalar multiplication
+        Vector3 operator*(float scalar) const
+        {
+            return Vector3(x * scalar, y * scalar, z * scalar);
+        }
 
+        //Dot product
+        float Dot(const Vector3 & other) const
+        {
+            return x * other.x + y * other.y + z * other.z;      
+        }
+
+        //Cross  product
+        Vector3 Cross(const Vector3& other) const
+        {
+            return Vector3(
+                y * other.z - z * other.y,
+                z * other.x - x * other.z,
+                x * other.y - y * other.x
+            );
+        }
+
+        // Magnitude
+        float Magnitude() const
+        {
+            return sqrt(x * x + y * y + z * z);
+        }
+
+        //Normalize
+        Vector3 Normalize() const
+        {
+            float mag = Magnitude();
+            if (mag == 0) return Vector3();
+            return Vector3(x / mag, y / mag, z / mag);
+        }
 	};
 }
+
+#endif // VECTOR3_H
