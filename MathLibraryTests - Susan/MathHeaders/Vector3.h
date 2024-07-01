324b3eeb13af5459bf9bdf3618b40078f47d06fd
Susan Achiech <127722179+SusanAchiech@users.noreply.github.com>
2024-07-01 10:26:18 +0300

    initializing constructors, equality and addition operators

--- MathLibraryTests - Susan/MathHeaders/Vector3.h
+++ MathLibraryTests - Susan/MathHeaders/Vector3.h
@@ -1,10 +1,29 @@
 #pragma once
+#include <string>
 
 namespace MathClasses
 {
     struct Vector3
     {
-        // Add the struct data fields and methods, as per the tutorials and exercises, here...
-        // ...
+        float x, y, z;
+
+        //constructors
+        Vector3(): x(0), y(0), z(0) {}
+        Vector3(float x, float y, float z ) : x(x), y(y), z(z) {}
+
+        //Ewuality operator
+        bool operator==(const Vector3& other) const
+        {
+            return x == other.x && y == other.y && z == other.z;
+        }
+
+        //Additional operator
+        bool operator+(const Vector3& other) const
+        {
+            return Vector3(x + other.x, y + other.y, z + other.z);
+        }
+
+        // Scalar multiplication
+
 	};
 }
